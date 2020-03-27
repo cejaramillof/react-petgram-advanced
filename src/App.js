b95ab1f2199e192ed9170d78dec3f692838e4929
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-03-26 21:54:50 -0500

    animations with keyframes styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import { GlobalStyle } from './GlobalStyle'
+import { GlobalStyle } from './styles/GlobalStyle'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 import { Logo } from './components/Logo'
--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,5 +1,24 @@
 import styled from 'styled-components'
+import { fadeIn } from '../../styles/animation'
 
+/*
+
+const fadeInKeyFrames = keyframes`
+  from {
+    filter: blur(5px);
+    opacity: 0;
+  }
+
+  to {
+    filter: blur(0);
+    opacity: 1;
+  }
+`
+
+export const fadeIn = ({ time = '1s', type = 'ease' } = {}) =>
+  css`animation: ${time} ${fadeInKeyFrames} ${type}`
+
+*/
 export const ImgWrapper = styled.div`
   border-radius: 10px;
   display: block;
@@ -11,6 +30,8 @@ export const ImgWrapper = styled.div`
 `
 
 export const Img = styled.img`
+  /* animation: 1s ${fadeInKeyFrames} ease; */
+  ${fadeIn({ time: '5s' })};
   box-shadow: 0 10px 14px rgba(0, 0, 0, .2);
   height: 100%;
   object-fit: cover;
