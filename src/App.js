1321e1f094c500850d48bd22c22b53bae859e140
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-03-26 21:08:48 -0500

    Global styled components and list of categories

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
 import React from 'react'
 import { Category } from './components/Category'
+import { GlobalStyle } from './GlobalStyle'
+import { ListOfCategories } from './components/ListOfCategories'
 
-export const App = () => (<Category />)
+export const App = () => (<><GlobalStyle /><Category /><ListOfCategories /></>)
