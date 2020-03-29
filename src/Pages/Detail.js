6242e5917fbbbf9b9e6033b4908a8bb71435ca3d
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-03-29 09:58:30 -0500

    fixing conflicts

--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -5,11 +5,13 @@ import { Layout } from '../components/Layout'
 export const Detail = ({ id }) => {
   return (
     <Layout title={`Fotografía ${id}`}>
-      {/*
-      <Helmet>
-        <title>Fotografía {id} | Petgram</title>
-      </Helmet>
-      */}
+      {
+        /*
+          <Helmet>
+          <title>Fotografía {id} | Petgram</title>
+          </Helmet>
+        */
+      }
       <PhotoCardWithQuery id={id} />
     </Layout>
   )
